96123920bf86c4a26bbb04ee6512bb9e7f7f0f13
cedXcoma <dr.bidule@outlook.com>
2023-03-10 12:25:14 -0500

    Initial commit

---
+++ Assets/_MyAssets/Script/Joueur.cs
@@ -0,0 +1,57 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Joueur : MonoBehaviour
+{
+    //Attributs
+    [SerializeField] private float _vitesse = 450;
+    private bool jouer;
+    private Rigidbody _rb;
+
+    // Méthodes privées
+    private void Start()
+    {
+       
+        jouer = true;
+        //Position de départ du joueur
+       // this.transform.position = new Vector3(-28f, 0.51f, -31f);
+        _rb = GetComponent<Rigidbody>();
+        //_rb.useGravity= true;
+    }
+
+    private void FixedUpdate()
+    {
+        if (jouer)
+        {
+            MovementsJoueur();
+        }
+
+    }
+
+
+    private void
+  
+    private void MovementsJoueur()
+    {
+
+        float positionX = Input.GetAxis("Horizontal");
+        float positionZ = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(positionX, 0f, positionZ);
+        //_rb.AddForce(poid * push, ForceMode.Acceleration);
+        _rb.velocity = direction * Time.fixedDeltaTime * _vitesse;
+
+        //_rb.AddForce(direction * Time.fixedDeltaTime * _vitesse); Fait comme un effet de glisse , car on utilise des forces pour se mouvoir donc pour arreter faut envoyer plus de force de l'autre coté
+    }
+    
+    public void Arret()
+    {
+        jouer = false;
+        if (!jouer)
+        {
+            Debug.Log("Vous avez terminé");
+            // OU faire this.gameobject.setActive(false)
+        }
+    }
+}
